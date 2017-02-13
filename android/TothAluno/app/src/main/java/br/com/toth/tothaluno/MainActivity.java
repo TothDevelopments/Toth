f2f9a9377e4f0492961f38873d945f4a7bbaa20b
Kennedy S. Dias <kennedysouzadias@gmail.com>
2017-02-13 19:42:28 -0200

    Aplicações em Android criadas.

---
+++ android/TothAluno/app/src/main/java/br/com/toth/tothaluno/MainActivity.java
@@ -0,0 +1,24 @@
+package br.com.toth.tothaluno;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.login);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void login(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
