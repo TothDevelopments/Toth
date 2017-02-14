21eda7f5c6a7959e984cf79dceb7efe0e3fbdf3a
Kennedy S. Dias <kennedysouzadias@gmail.com>
2017-02-14 21:39:49 -0200

    Android: Desenvolvendo Bottom navigation

--- android/TothAluno/app/src/main/java/br/com/toth/tothaluno/MainActivity.java
+++ android/TothAluno/app/src/main/java/br/com/toth/tothaluno/MainActivity.java
@@ -1,20 +1,50 @@
 package br.com.toth.tothaluno;
 
 import android.content.Intent;
+import android.support.annotation.IdRes;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.BottomBarTab;
+import com.roughike.bottombar.OnTabSelectListener;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        setContentView(R.layout.activity_login);
 
-//        Intent intent = new Intent(this, LoginActivity.class);
-//        startActivity(intent);
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+
+        final BottomBarTab tabAtividades = bottomBar.getTabWithId(R.id.tab_atividades);
+        final BottomBarTab tabCalendario = bottomBar.getTabWithId(R.id.tab_calendario);
+        final BottomBarTab tabChats = bottomBar.getTabWithId(R.id.tab_chats);
+        final BottomBarTab tabOpcoes = bottomBar.getTabWithId(R.id.tab_opcoes);
+
+        tabChats.setBadgeCount(10);
+
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                if (tabId == R.id.tab_atividades) {
+                    tabAtividades.removeBadge();
+                } else if (tabId == R.id.tab_calendario) {
+                    tabCalendario.removeBadge();
+                } else if (tabId == R.id.tab_chats) {
+                    tabChats.removeBadge();
+                } else if (tabId == R.id.tab_opcoes) {
+                    tabOpcoes.removeBadge();
+                }
+            }
+        });
+
+
+
     }
 
     public void login(View view){
