b4cc943242fbe8c65b461f120b497b82ddcd4c3c
Felipe Porfirio <felrpo@gmail.com>
2017-02-25 19:34:52 -0300

    Android Aluno: Notificações

--- android/TothAluno/app/src/main/java/br/com/tothprofessor/tothaluno/NotificacoesActivity.java
+++ android/TothAluno/app/src/main/java/br/com/tothprofessor/tothaluno/NotificacoesActivity.java
@@ -1,11 +1,8 @@
 package br.com.tothprofessor.tothaluno;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+
 
 /**
  * Created by Felipe on 25/02/2017.
@@ -13,14 +10,6 @@ import android.widget.TextView;
 
 public class NotificacoesActivity  extends AppCompatActivity {
 
-
-    private TextView textViewAtividadeNota;
-    private TextView textViewNomeAtividade;
-    private TextView textViewDataEntrega;
-    private TextView textViewTempoatividade;
-    private TextView textViewIntroducaoAtividade;
-    private Button buttonVerCorrecao;
-    private LinearLayout backgroundAtividadeNota;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
