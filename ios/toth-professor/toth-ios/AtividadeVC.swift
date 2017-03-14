efd61b1516aa6d03bcf5f326afce88f04aab6d21
Danillo Lange <danillo_lange@hotmail.com>
2017-03-14 19:04:23 -0300

    Formulario Removido

--- ios/toth-professor/toth-ios/AtividadeVC.swift
+++ ios/toth-professor/toth-ios/AtividadeVC.swift
@@ -20,21 +20,12 @@ class AtividadeVC: UIViewController {
     
     @IBOutlet weak var descricaoAtividade: UITextView!
     
-    var listaAtividades = Atividade()
-    
     var date = NSDate()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        listaAtividades = Atividade(atividadeNome: "Atividade 1", atividadeImg: "dani_teste", atividadeInicio: "20/02/2017", atividadeDescricao: "Esta atividade é essencial para o seu desenvolvimento profissional, social e pessoal.", atividadeEntrega: "23/03/2017", atividadeTempo: "50", atividadeTipo: "Tarefa")
-        
         //imgAtividade = listaAtividades.atividadeImg
-        
-        nomeAtividade.text = listaAtividades.atividadeNome
-        entregaAtividade.text = listaAtividades.atividadeEntrega
-        descricaoAtividade.text = listaAtividades.atividadeDescricao
-        tempoAtividade.text = listaAtividades.atividadeTempo
         
         // Do any additional setup after loading the view.
     }
