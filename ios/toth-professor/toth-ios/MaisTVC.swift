d2802a9e812dddd19b285ca98078148ac1d4188c
Andre Rosa <andre.i.r@live.com>
2017-03-23 22:00:28 -0300

    ajustes de layout

--- ios/toth-professor/toth-ios/MaisTVC.swift
+++ ios/toth-professor/toth-ios/MaisTVC.swift
@@ -13,17 +13,7 @@ class MaisTVC: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    }
-
-   
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-      
-       // performSegue(withIdentifier: "listaAlunosSG", sender: self)
+       self.tableView.tableFooterView = UIView(frame: .zero)
     }
 
 }
